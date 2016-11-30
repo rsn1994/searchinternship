68424916e1a18d56652b721bb27eeae262111a54
Rakesh s nair <rsn4rsn@gmail.com>
2016-11-30 19:32:31 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "localhost";
+$username = "id89978_humanize";
+$password = "rsn@1994";
+$dbname = "id89978_data";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+?>
